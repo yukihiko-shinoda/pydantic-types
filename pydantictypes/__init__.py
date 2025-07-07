0becb5d56e7c5020c2d656c71b1878d3c6f51f0e
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2025-07-07 12:26:26 +0900

    Bump version: 0.1.0 → 1.0.0

--- pydantictypes/__init__.py
+++ pydantictypes/__init__.py
@@ -7,7 +7,7 @@ from pydantictypes.string_with_comma_to_int import *  # noqa: F403
 from pydantictypes.string_with_comma_to_optional_int import *  # noqa: F403
 from pydantictypes.symbol_yen_string_to_int import *  # noqa: F403
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 __all__ = []
 __all__ += kanji_yen_string_to_int.__all__  # type:ignore[name-defined] # noqa: F405 pylint: disable=undefined-variable
